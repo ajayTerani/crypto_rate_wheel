c11c6a20b8a037afa17b1be942d404031c33d701
ajayTerani <ajayTerani67@gmail.com>
2024-03-04 21:54:56 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cryptoratewheel/utils/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const CryptoApp());
 }
 
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,4 @@
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 Future<bool> checkInternetConnectivity() async {
